fb74ba7449ed25a78c3e93b71e3231ac86cad2ae
jovanetz <78218096+jovanetz@users.noreply.github.com>
2023-04-17 05:35:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function realtimeClock() {
+  var rtClock = new Date();
+
+  var hours = rtClock.getHours();
+  var minutes = rtClock.getMinutes(); 
+  var seconds = rtClock.getSeconds();
+
+  hours = ("0" + hours).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+  seconds = ("0" + seconds).slice(-2);
+
+  document.getElementById('clock').innerHTML =
+  hours + ":" + minutes + ":" + seconds;
+  var t = setTimeout(realtimeClock, 500);
+
+  document.getElementById('clock').innerHTML =
+  new Date ().toLocaleString("en-US",{timeZone: 'Brazil/East', timeStyle: 'medium', hourCycle: 'h24'});
+
+}
